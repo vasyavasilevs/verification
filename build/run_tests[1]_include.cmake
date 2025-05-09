fde528f41d33437cd8782ca136521f2f8c520559
Valentina Goncharenko <goncharenko.vp@ispras.ru>
2025-05-10 01:22:50 +0300

    added Cmake, 1 test failed because of order

---
+++ build/run_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/vasyavasilevs/verification/build/run_tests[1]_tests.cmake")
+  include("/home/vasyavasilevs/verification/build/run_tests[1]_tests.cmake")
+else()
+  add_test(run_tests_NOT_BUILT run_tests_NOT_BUILT)
+endif()
